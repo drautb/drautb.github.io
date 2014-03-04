19c5e38578472bc7ab2a0f67ac6600837d8beac7
Ben Draut <drautb@gmail.com>
2014-03-04 16:58:59 -0700

    Added auto-refresh for stocks

--- stocks/stocks.js
+++ stocks/stocks.js
@@ -68,7 +68,7 @@ stocksApp.controller('teamCtrl', ['$scope', '$interval', '$http', function($scop
 	$scope.latestQuotes = [];
 
 	getLatestQuotes();
-	// $interval(getLatestQuotes, 30000);
+	$interval(getLatestQuotes, 10000);
 
 	function getLatestQuotes() {
 		var url = 'http://query.yahooapis.com/v1/public/yql';
