3b70b243b919d9dd91623635a3b98082ae9e266c
Ben Draut <drautb@gmail.com>
2019-09-28 12:34:51 -0600

    Testing React App

---
+++ myapp/precache-manifest.95bb64e4ae56a859ab9a3172a32a8f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3fd4b0e11d80f8a0f1bd75812fc34f",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "7e1edaa4e18cdf551f80",
+    "url": "/myapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5e4c5fd6b8dca6cb8574",
+    "url": "/myapp/static/js/2.47257b23.chunk.js"
+  },
+  {
+    "revision": "7e1edaa4e18cdf551f80",
+    "url": "/myapp/static/js/main.09ab8426.chunk.js"
+  },
+  {
+    "revision": "531475fc43af4caa365b",
+    "url": "/myapp/static/js/runtime-main.76726000.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/myapp/static/media/logo.25bf045c.svg"
+  }
+]);
